5d58287a1f60375443d3635c4c0088e20b76449e
Liz Fransen <elizabethlfransen@gmail.com>
2022-09-28 18:28:56 -0500

    fix getCapitalizedName

--- src/main/java/io/github/elizabethlfransen/secretlycomplicated/material/SCMaterial.java
+++ src/main/java/io/github/elizabethlfransen/secretlycomplicated/material/SCMaterial.java
@@ -4,9 +4,11 @@ import io.github.elizabethlfransen.secretlycomplicated.materialform.MaterialForm
 import io.github.elizabethlfransen.secretlycomplicated.materialform.MaterialFormFactory;
 import io.github.noeppi_noeppi.libx.mod.registration.Registerable;
 import net.minecraft.resources.ResourceLocation;
-import org.apache.commons.lang3.text.WordUtils;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class SCMaterial implements Registerable {
@@ -32,10 +34,10 @@ public class SCMaterial implements Registerable {
     }
 
     public String getCapitalizedName() {
-        return Arrays.stream(name.split("/_/g"))
+        return Arrays.stream(name.split("_"))
                 .filter(part -> part.length() > 0)
                 .map(part -> Character.toUpperCase(part.charAt(0)) + part.substring(1))
-                .collect(Collectors.joining());
+                .collect(Collectors.joining(" "));
     }
 
     @Override
